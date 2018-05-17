70210435f0a0b7106c8bf1414cb626ed35ade40b
neil patel <neil@myindianweddingapp.com>
2018-05-17 10:26:27 +0100

    Added attributes and created db

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ var Alexa = require("alexa-sdk");
 exports.handler = function(event, context,callback){
   var alexa = Alexa.handler(event, context, callback);
   alexa.dynamoDBTableName = "MyThingsAndPlaces";
-  alexa.appId = "amzn1.ask.skill.c32f8215-aa34-47fb-9c94-d8cf0cae0e97";
+  alexa.appId = "";
   alexa.registerHandlers(Handler);
   alexa.execute();
-}
+};
 
-var Hander = {
+var Handler = {
   "LaunchRequest" : function() {
-    this.emit(":askWithCard", "Hi you can ask me to remember your thing and place", "Hi you can ask me to remember your thing and place," "Remember Skill", "Hi you can ask me to remember your thing and place");
+    this.emit(":askWithCard", "Hi you can ask me to remember your thing and place", "Hi you can ask me to remember your thing and place," ,"Remember Skill", "Hi you can ask me to remember your thing and place");
   },
   "AddThingsIntent": function() {
     var ThingName = this.event.request.intent.slots.ThingName.value;
     var PlaceName = this.event.request.intent.slots.PlaceName.value;
+    if(this.attributes.Place === undefined){
+      this.attributes.Place = {};
+    }
+    this.attributes.Place[ThingName] = PlaceName;
+    this.emit(':tell', "I will remember that your " + ThingName + " is in your " + PlaceName);
   }
-}
+};
